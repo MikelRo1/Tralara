69c84a7ded9b3dd65457d5398053534e004b1891
ALUMNO <euskadi612>
2017-05-25 20:23:46 +0200

    Fachadatan kanbiok

--- src/server/remote/IUsuarioF.java
+++ src/server/remote/IUsuarioF.java
@@ -1,11 +1,12 @@
 package server.remote;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 import server.data.dto.UsuarioDTO;
 
-public interface IUsuarioF {
+public interface IUsuarioF extends Remote{
 	
 	public boolean checkAddUser(String user) throws RemoteException;
 	public ArrayList<UsuarioDTO> getUsuarioDTO() throws RemoteException;
